8788d9a2a5066ea749f16cc2b2776923c729e570
jinlee4140 <jinlee4140@gmail.com>
2015-09-03 23:07:14 -0400

    what if

---
+++ python29.py
@@ -0,0 +1,23 @@
+people = 20
+cats = 30
+dogs = 15
+
+if people < cats:
+	print "Too many cats!!!!"
+if people > cats:
+	print "Not many cats!!!"
+if people < dogs:
+	print "too many dogs!!!!!"
+if people > dogs:
+	print "Not many dogs!!!!"
+
+dogs += 5
+
+if people >= dogs:
+	print "People are greater than or equal to dogs"
+if people <= dogs:
+	print "people are less than or equal to dogs"
+
+if people == dogs:
+	print "People are equal to dogs"
+
