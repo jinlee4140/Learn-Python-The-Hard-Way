81cffe7787e26cefe10e33b37a040da78ad841b2
jinlee4140 <jinlee4140@gmail.com>
2015-09-03 22:35:01 -0400

    print first and last

--- python25.py
+++ python25.py
@@ -19,3 +19,25 @@ print word1
 #the first index in the array has been removed
 
 
+def print_last_word(words):
+	word = words.pop(-1)
+	print word
+
+print print_last_word(word1)
+print word1
+
+
+sentence1 = "james is just too handsome too beautiful"
+def sort_sentence(sentence):
+	word = break_words(sentence)
+	return sorted_words(word)
+
+print sort_sentence(sentence1)
+
+def print_first_and_last(sentence):
+	words = break_words(sentence)
+	print_first_word(words)
+	print_last_word(words)
+
+print print_first_and_last(sentence1)
+
